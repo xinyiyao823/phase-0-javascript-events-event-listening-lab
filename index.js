6162b8d4dd0aa3a5a71157d64907a7cdc7e27112
Xinyi <xinyiyao84@yahoo.com>
2021-09-05 20:57:27 -0500

    commit tree b7479a83bd805d0c03b0ff4241c0aa57ffc061ad

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-    alert('I was clicked!');
+function addingEventListener() { //define function to handle event for input
+    const input = document.getElementById('input'); // first grab element by id "input" and save it in variable
+    input.addEventListener('click', function() { // add event listener to listen for "click", and pass in callback fx
+    alert('I was clicked!'); //then display "i was clicked!" 
 }); 
 }
+
+addingEventListener(); //invoke the function again to activate event listener
